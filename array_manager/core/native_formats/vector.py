eb9e15aa72ee496c98b2dc17af91d2442c147580
Anugrah Jo Joshy <ajoshy@eng.ucsd.edu>
2021-04-09 09:51:08 -0700

    Vector class added with append and dunderscore methods

--- array_manager/core/native_formats/vector.py
+++ array_manager/core/native_formats/vector.py
@@ -1,5 +1,6 @@
 """Define the Vector class"""
 import numpy as np
+import scipy.sparse as sp
 
 
 class Vector(object):
@@ -31,6 +32,7 @@ class Vector(object):
         else:
             self.data = data = np.zeros(self.vector_components_dict.vector_size)
 
+        self.setup_views_ = setup_views
         if setup_views:
             self.dict_ = self.setup_views(self.vector_components_dict)
 
@@ -64,11 +66,211 @@ class Vector(object):
     def __len__(self):
         return self.vector_components_dict.vector_size
 
+    def append(self, other):
+        new_vector_components_dict = {**self.vector_components_dict, **other.vector_components_dict}
+        new_vector = Vector(new_vector_components_dict)
+        new_vector.allocate(data=np.append(self.data, other.data), setup_views=self.setup_views_)
+        
+        return new_vector
+
+    def check_type_and_size(self, other):
+        if isinstance(other, (int, float)):
+            pass
+        elif isinstance(other, Vector):
+            if len(other) != len(self) :
+                raise TypeError('Arguments should be objects of the Vector/numpy.ndarray class with equal sizes')
+        elif isinstance(other, np.ndarray):
+            if len(other.shape) != 1:
+                raise TypeError('Objects of the numpy.ndarray should be vectors')
+            if len(other) != len(self) :
+                raise TypeError('Arguments should be objects of the Vector/numpy.ndarray class with equal sizes')
+        else:
+            raise TypeError('Argument should be either an object of the Vector/numpy.ndarray class or a scalar (int or float)')
+
+        
+
     def __iadd__(self, other):
-        self.data += other.data
+        self.check_type_and_size(other)
+        if isinstance(other, Vector):
+            self.data += other.data
+        else:                    # isinstance(other, (int, float, np.ndarray))
+            self.data += other
 
     def __isub__(self, other):
-        self.data -= other.data
+        self.check_type_and_size(other)
+        if isinstance(other, Vector):
+            self.data -= other.data
+        else:
+            self.data -= other
+
+    def __imult__(self, other):
+        self.check_type_and_size(other)  
+        if isinstance(other, Vector):
+            self.data *= other.data
+        else:
+            self.data *= other
+    
+    def __itruediv__(self, other):
+        self.check_type_and_size(other)
+        if isinstance(other, Vector):
+            self.data /= other.data
+        else:
+            self.data /= other
+
+    def __ipow__(self, other):
+        self.check_type_and_size(other)
+        if isinstance(other, Vector):
+            self.data **= other.data
+        else:
+            self.data **= other
+        
+
+    # In-place matrix multiplication is not (yet) supported. Use 'a = a @ b' instead of 'a @= b'.
+    
+    # def __imatmul__(self, other):
+    #     if other.dense_shape
+    #     if isinstance(other, Matrix):
+    #         self.data @= other.data
+    #     else:
+    #         raise TypeError('Argument should be either an object of the Vector class or a scalar (int or float)')
+
+    def __add__(self, other):
+        self.check_type_and_size(other)
+        new_vector = Vector(self.vector_components_dict)
+
+        if isinstance(other, Vector):
+            new_data = self.data + other.data
+        else:                   # isinstance(other, (int, float, np.ndarray))
+            new_data = self.data + other
+        
+        new_vector.allocate(data=new_data, setup_views=self.setup_views_)
+        return new_vector
+
+    def __sub__(self, other):
+        self.check_type_and_size(other)
+        new_vector = Vector(self.vector_components_dict)
+        
+        if isinstance(other, Vector):
+            new_data = self.data - other.data
+        else:
+            new_data = self.data - other
+
+        new_vector.allocate(data=new_data, setup_views=self.setup_views_)
+        return new_vector
+
+    def __mult__(self, other):
+        self.check_type_and_size(other)  
+        new_vector = Vector(self.vector_components_dict)
+        
+        if isinstance(other, Vector):
+            new_data = self.data * other.data
+        else:
+            new_data = self.data * other
+
+        new_vector.allocate(data=new_data, setup_views=self.setup_views_)
+        return new_vector
+    
+    def __truediv__(self, other):
+        self.check_type_and_size(other)
+        new_vector = Vector(self.vector_components_dict)
+        
+        if isinstance(other, Vector):
+            new_data = self.data / other.data
+        else:
+            new_data = self.data / other
+
+        new_vector.allocate(data=new_data, setup_views=self.setup_views_)
+        return new_vector
+
+    def __pow__(self, other):
+        self.check_type_and_size(other)
+        new_vector = Vector(self.vector_components_dict)
+        
+        if isinstance(other, Vector):
+            new_data = self.data ** other.data
+        else:
+            new_data = self.data ** other
+
+        new_vector.allocate(data=new_data, setup_views=self.setup_views_)
+        return new_vector
+
+    def __matmul__(self, other):  # (Note: Vector object is the first argument)
+        """
+        Returns a scalar, a numpy array (vector), or a Vector object that results from the given matrix multiplication.
+        """
+        if not(isinstance(other, (Vector, Matrix, np.ndarray, DenseMatrix, COOMatrix, CSRMatrix, CSCMatrix, sp.coo.coo_matrix, sp.csr.csr_matrix, sp.csc.csc_matrix))):
+            raise TypeError('Arguments should be objects of Vector, DenseMatrix, COOMatrix, CSRMatrix, CSCMatrix, sp.coo.coo_matrix,sp.csr.csr_matrix, or sp.csc.csc_matrix  classes')
+        
+        # Vector inner product
+        if isinstance(other, Vector):
+            if len(other) != len(self):
+                raise TypeError('Arguments should be objects of the Vector/numpy.ndarray class with equal sizes')
+            else:
+                inner_product = self.data @ other.data
+                
+            return inner_product
+
+        # Inner product with np.ndarray 
+        elif isinstance(other, np.ndarray):
+            if len(other.shape) not in (1, 2):
+                raise TypeError('Objects of the numpy.ndarray should be vectors/matrices')
+            
+            # numpy vector inner product
+            if len(other.shape) == 1:
+                if len(other) != len(self) :
+                    raise TypeError('Arguments should be objects of the Vector/numpy.ndarray class with equal sizes')
+                else:
+                    inner_product = self.data @ other
+
+            # numpy matrix inner product
+            else:                             # len(other.shape) = 2
+                if len(self) != other.shape[0] :
+                    raise TypeError('Arguments should have compatible shapes')
+                else:
+                    inner_product = self.data @ other
+
+            return inner_product
+
+        # scipy sparse matrix inner product
+        elif isinstance(other, (sp.coo.coo_matrix, sp.csr.csr_matrix, sp.csc.csc_matrix)):
+            if len(self) != other.shape[0] :
+                raise TypeError('Arguments should have compatible shapes')
+            else:
+                inner_product = self.data @ other
+
+            return inner_product
+
+        # Matrix inner product (only case that returns a Vector object)
+        elif isinstance(other, (Matrix, DenseMatrix, COOMatrix, CSRMatrix, CSCMatrix)):
+            if other.dense_shape[0] != len(self):
+                raise TypeError('Arguments should have compatible shapes')
+            else:
+                if isinstance(other, Matrix): 
+                    inner_product = Vector(other.matrix_components_dict.vector_components_dict2)
+                else:
+                    inner_product = Vector(other.native.matrix_components_dict.vector_components_dict2)
+      
+                if isinstance(other, DenseMatrix):
+                    new_data = self.data @ other.data
+                elif isinstance(other, Matrix):
+                    scipy_matrix = sp.coo_matrix((other.vals.data, (other.rows, other.cols)), shape=other.dense_shape)
+                    new_data = self.data @ scipy_matrix
+                elif isinstance(other, COOMatrix):
+                    scipy_matrix = sp.coo_matrix((other.data, (other.rows, other.cols)), shape=other.dense_shape)
+                    new_data = self.data @ scipy_matrix
+                elif isinstance(other, CSRMatrix):
+                    scipy_matrix = sp.csr_matrix((other.data, other.cols, other.indptr), shape=other.dense_shape)
+                    new_data = self.data @ scipy_matrix
+                else:
+                    scipy_matrix = sp.csc_matrix((other.data, other.rows, other.indptr), shape=other.dense_shape)
+                    new_data = self.data @ scipy_matrix
+
+        if isinstance(other, Matrix): 
+            inner_product.allocate(data=new_data, setup_views=other.matrix_components_dict.vector_components_dict2.setup_views_)
+        else:
+            inner_product.allocate(data=new_data, setup_views=other.native.matrix_components_dict.vector_components_dict2.setup_views_)
+
+        return inner_product
 
         # vec == other
         # vec.set(other)
