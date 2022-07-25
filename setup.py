24b5338b9eb23207817eed403269290cabca18c5
anugrahjo <44123535+anugrahjo@users.noreply.github.com>
2022-07-25 12:35:33 -0700

    Dash dependency removed

--- setup.py
+++ setup.py
@@ -9,8 +9,6 @@ setup(
     install_requires=[
         'numpy',
         'scipy',
-        'dash==1.2.0',
-        'dash-daq==0.1.0',
         'pint',
         'sphinx-rtd-theme',
         'sphinx-code-include',
