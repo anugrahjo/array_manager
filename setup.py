1d1ee1fca13afcf58ef967ab2bafb8effd20ea66
anugrahjo <44123535+anugrahjo@users.noreply.github.com>
2023-01-27 01:03:26 -0800

    Unwanted dependencies removed, find_packages added

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='array_manager',
     version='0.1',
-    packages=[
-        'array_manager',
-    ],
+    packages=find_packages(),
+    #packages=['array_manager'],
     install_requires=[
         'numpy',
         'scipy',
         'pint',
-        'sphinx-rtd-theme',
-        'sphinx-code-include',
-        'jupyter-sphinx',
-        'numpydoc',
+#         'sphinx-rtd-theme',
+#         'sphinx-code-include',
+#         'jupyter-sphinx',
+#         'numpydoc',
     ],
 )
 
